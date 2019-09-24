f8a0e5b07b681481e40bdd74f5e043357fdfe0d2
Heli Sivunen <heli@mooncat.tv>
2019-09-24 16:15:28 +0100

    Refactor.

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,14 +1,13 @@
 # frozen_string_literal: true
 
 class Oystercard
-  attr_accessor :balance, :in_journey, :entry_station
+  attr_accessor :balance, :entry_station
 
   MAX_BALANCE = 90
   MIN_FARE = 1 # min_fare would be equivalent to min_balance
 
   def initialize
     @balance = 0
-    @in_journey = false
   end
 
   def top_up(amount)
@@ -20,17 +19,19 @@ class Oystercard
   def touch_in(station)
     raise 'Insufficient funds to travel.' if insufficient_balance?
 
-    self.in_journey = true
     @entry_station = station
+    in_journey?
   end
 
   def touch_out
-    self.in_journey = false
     deduct(MIN_FARE)
+    @entry_station = nil
+    !in_journey?
   end
 
   def in_journey?
-    in_journey
+    return true if entry_station
+    false
   end
 
   private
