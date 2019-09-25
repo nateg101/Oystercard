e1d8f1bc9d71169e3a809cec368bf06a7a73b3cf
Nat Elliott-Green <natelliottgreen34@gmail.com>
2019-09-25 13:07:46 +0100

    Created a station class and spec

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -4,13 +4,13 @@ require 'station'
 describe Station do
   # subject(:station) { described_class.new(name, zone) }
 
-    subject { described_class.new(name: "Aldgate", zone: 2)}
+    subject { described_class.new("Aldgate", 2)}
 
     it 'knows it has a name' do
-      expect(station.name).to eq "Aldgate"
+      expect(subject.name).to eq "Aldgate"
     end
 
     it 'knows what zone it is in' do
-      expect(station.zone).to eq 2
+      expect(subject.zone).to eq 2
     end
 end
